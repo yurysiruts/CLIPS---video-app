cf84dc3311790074c7f3bfd07ff53d35f396e238
yurysiruts <sirut.2003@gmail.com>
2022-02-24 23:52:55 +0100

    initial commit with auth-modal, shared modal, nav

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkmode: false, // or 'media' or 'class'
+  // content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
